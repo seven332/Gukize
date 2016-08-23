ea8a68149a2732e0f27926a431bf9492f58e49c0
seven332 <seven332@163.com>
2016-08-24 01:52:19 +0800

    Update example

--- app/src/main/java/com/hippo/gukize/example/MainActivity.java
+++ app/src/main/java/com/hippo/gukize/example/MainActivity.java
@@ -16,6 +16,7 @@
 
 package com.hippo.gukize.example;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,8 +25,14 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import com.hippo.gukize.GukizeView;
+import com.hippo.konwidget.AdvImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL_JPEG = "https://www.gstatic.com/webp/gallery/1.sm.jpg";
+    private static final String URL_PNG = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png";
+    private static final String URL_GIF = "https://s3.amazonaws.com/giphygifs/media/4aBQ9oNjgEQ2k/giphy.gif";
+    private static final String URL_BAD = "https://sdarea.com/safrreedtger.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +58,48 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
+                GukizeView image;
                 if (convertView == null) {
                     convertView = new GukizeView(MainActivity.this);
-                    convertView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 512));
+                    Resources resources = getResources();
+                    image = (GukizeView) convertView;
+                    image.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 512));
+                    image.setPlaceholderDrawable(resources.getDrawable(R.drawable.placeholder));
+                    image.setFailureDrawable(resources.getDrawable(R.drawable.failure));
+                    image.setRetryType(GukizeView.RETRY_TYPE_CLICK);
+                } else {
+                    image = (GukizeView) convertView;
                 }
-                GukizeView image = (GukizeView) convertView;
-                image.load("gif", "https://s3.amazonaws.com/giphygifs/media/4aBQ9oNjgEQ2k/giphy.gif");
+
+                switch (position % 5) {
+                    case 0:
+                        image.load("jpeg", URL_JPEG);
+                        break;
+                    case 1:
+                        image.load("png", URL_PNG);
+                        break;
+                    case 2:
+                        image.load("gif", URL_GIF);
+                        break;
+                    case 3:
+                        image.load("gif", URL_GIF);
+                        break;
+                    case 4:
+                        image.load("bad", URL_BAD);
+                        break;
+                }
+
+                switch (position % 2) {
+                    case 0:
+                        image.setActualScaleType(AdvImageView.SCALE_TYPE_CENTER);
+                        image.setFailureScaleType(AdvImageView.SCALE_TYPE_FIT_XY);
+                        break;
+                    case 1:
+                        image.setActualScaleType(AdvImageView.SCALE_TYPE_FIT_XY);
+                        image.setFailureScaleType(AdvImageView.SCALE_TYPE_CENTER);
+                        break;
+                }
+
                 return convertView;
             }
         });
